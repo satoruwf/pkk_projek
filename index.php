031d9b0d536be5119cf44038c0f13a9cf8b23015
satoruwf <parklya13@gmail.com>
2024-10-09 10:53:46 +0700

    first

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+    <title> Registration </title>
+</head>
+<body>
+   
+<h1> Halaman Registrasi </h1>
+
+<form action="" method="post">
+
+    <ul>
+        <li>
+            <label for="username">Username :</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">Password :</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <label for="email">Email :</label>
+            <input type="email" name="email" id="email">
+        </li>
+    </ul>
+</form>
+
+
+</body>
+</html>
