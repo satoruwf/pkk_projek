5e9e1657408f4c6f39bbf11286d17062c2d4e55f
satoruwf <parklya13@gmail.com>
2024-10-09 11:13:26 +0700

    perubahan1

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<head>
-    <title> Registration </title>
-</head>
-<body>
-   
-<h1> Halaman Registrasi </h1>
-
-<form action="" method="post">
-
-    <ul>
-        <li>
-            <label for="username">Username :</label>
-            <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="password">Password :</label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-            <label for="email">Email :</label>
-            <input type="email" name="email" id="email">
-        </li>
-    </ul>
-</form>
-
-
-</body>
-</html>
+<?php
+    $koneksi = mysqli_connect("localhost", "root", "", "dbaya_clothes")
+?>
